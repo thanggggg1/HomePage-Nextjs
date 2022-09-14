db8d68997c687fda1637ac11e525ba31f1213595
thanggggg1 <thang.lq194838@sis.hust.edu.vn>
2022-09-14 19:37:22 +0700

    push

--- components/HomePage/CardFeedback.tsx
+++ components/HomePage/CardFeedback.tsx
@@ -2,39 +2,49 @@ import {memo} from "react";
 import styled from "styled-components";
 import Image from "next/image";
 import {IC_CONTENT_01, IC_FIVE_STAR} from "../../assets";
-import {RowSection} from "../../pages";
 import {LeftDiv} from "../HomePage/CardContentSection";
 import {fontScale} from "../../utils/fontScale";
 
-export const CardFeedback = memo(function CardFeedback(){
+
+const NUM_OF_LINES=4
+
+interface CardFeedbackProps {
+    name: string;
+    iconUri: string;
+    content: string;
+}
+
+export const CardFeedback = memo(function CardFeedback(props: CardFeedbackProps) {
+    const {name, iconUri, content} = props;
     return (
-<Container>
-    <LeftDiv>
-        <Image src={IC_CONTENT_01}/>
-        <TextHeading>Store A</TextHeading>
-    </LeftDiv>
-    <br/>
-    <LeftDiv>
-        <Image src={IC_FIVE_STAR}/>
-    </LeftDiv>
-    <br/>
-    <TextNormal>We provide apps that deal with daily struggles of a merchant.</TextNormal>
-</Container>
+        <Container>
+            <LeftDiv>
+                <Image src={IC_CONTENT_01}/>
+                <TextHeading>{name}</TextHeading>
+            </LeftDiv>
+            <br/>
+            <LeftDiv>
+                <Image src={IC_FIVE_STAR}/>
+            </LeftDiv>
+            <br/>
+            <ContentDiv>
+                <TextNormal>{content}</TextNormal>
+            </ContentDiv>
+        </Container>
     )
 })
 const Container = styled.div`
-display: flex;
+  display: flex;
   flex-direction: column;
   align-items: center;
-  padding:40px 20px;
+  padding: 40px 20px;
   background-color: #F6F6F7;
   border-radius: 6px;
-;
 `
 const TextHeading = styled.span`
   font-style: normal;
   font-weight: 600;
-  font-size: ${p=> fontScale(28)}px;
+  font-size: ${p => fontScale(28)}px;
   line-height: 38px;
   letter-spacing: 0.025em;
   color: #1D1B29;
@@ -42,9 +52,18 @@ const TextHeading = styled.span`
 const TextNormal = styled.span`
   font-style: normal;
   font-weight: 600;
-  font-size: ${p=> fontScale(16)}px;
+  font-size: ${p => fontScale(16)}px;
   line-height: 22px;
   letter-spacing: 0.025em;
   color: #1D1B29;
 `
-
+const ContentDiv = styled.div`
+  word-break: break-word;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  line-height: 16px; /* fallback */
+  max-height: 120px; /* fallback */
+  -webkit-line-clamp: 4; /* number of lines to show */
+  -webkit-box-orient: vertical;
+`
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import {ButtonNavBar, NavBar, TextButtonWhite} from "../components/NavBar";
 import {
     IMG_DIVIDE_SECTION,
-    IMG_HEADER,
+    IMG_HEADER, IMG_LOGO,
     IMG_REACTANGLE_HOME_01,
     IMG_REACTANGLE_HOME_02,
     IMG_REACTANGLE_HOME_03
@@ -21,6 +21,7 @@ import {fontScale} from "../utils/fontScale";
 import {DivImage} from "./Products/FreeGiftPage";
 import Head from "next/head";
 import UseWindowSize from "../utils/useWindowSize";
+
 
 const Home: NextPage = memo(function Home() {
     const {width} = UseWindowSize();
@@ -115,22 +116,34 @@ const Home: NextPage = memo(function Home() {
                         </StatisticalWrapper>
                     </DivSpaceBetWeen>
                 </WrapperPaddingFeedback>
-                <Marquee speed={40}>
+                <Marquee speed={40} style={{flex:1,display:'flex'}}>
                     <FeedbackCardWrapper>
                         <CardFeedbackItem>
-                            <CardFeedback/>
+                            <CardFeedback name={'PIPCORN'} iconUri={IMG_LOGO} content={'The Free Gift app from Sortecom is a fantastic tool that we use to launch more complex gift-with-purchase promotions. All of the promotions we want to run on it are simple to navigate, and I offer this app 5 stars for its best customer service! Despite our 14-hour time difference, they always answer quickly to my questions, assist me in setting up campaigns, and help us in customizing pop-ups to match our branding and style. Since I started working with Sortecom recently, they have been incredibly helpful in finding solutions to our issues and patiently explaining everything to me because I have a lot of questions. My recommendation is to ask for assistance if you run into problems or if you don\'t like the way something looks. They are so supportive and helpful. What a wonderful experience!'}/>
                         </CardFeedbackItem>
                         <CardFeedbackItem>
-                            <CardFeedback/>
+                            <CardFeedback name={'TRIANGL'} iconUri={IMG_LOGO} content={'Sortecom continuously helps us increase our sales and conversion rates. Especially, their dashboard and data are very easy to understand, and the implementation is also easy and seamless. I really recommend Sortecom since they seem to have the best apps which help optimize sales.'}/>
                         </CardFeedbackItem>
                         <CardFeedbackItem>
-                            <CardFeedback/>
+                            <CardFeedback name={'LeatherHead'} iconUri={IMG_LOGO} content={'These apps are incredible! My conversion rate went up by almost 15% in the first month of using it. I have always been welcomed like a VIP by the staff, who have been really kind and helpful. I highly suggest the app to all Shopify\'s merchants.'}/>
                         </CardFeedbackItem>
                         <CardFeedbackItem>
-                            <CardFeedback/>
+                            <CardFeedback name={'BREMONT'} iconUri={IMG_LOGO} content={'Best support ever, out-of-the-box, creative, and really great set of tools to help you do whatever you need to run your marketing campaigns and protect them. It’s truly affordable. Please be active with the support team. They won’t hesitate to help you immediately.'}/>
                         </CardFeedbackItem>
                         <CardFeedbackItem>
-                            <CardFeedback/>
+                            <CardFeedback name={'BITE'} iconUri={IMG_LOGO} content={'Sortecom\'s apps were recommended to me as an all-in-one marketing place. I am new to having an eCommerce store so I was grateful to find them. And relieved at the thought of spending less time researching a bunch of supportive apps while having so many other things to learn and focus on. And hopeful that it actually works well so I can keep it simple. So far it\'s been fun to see the different apps work together as I build my store. I have sometimes found the supporter and was guided quickly and thoughtfully. I truly appreciate this support. I know now that there are really helpful very short informative tutorials on each app that would have kept me from even needing help so I would recommend starting there.'}/>
+                        </CardFeedbackItem>
+                        <CardFeedbackItem>
+                            <CardFeedback name={'New Market Goods'} iconUri={IMG_LOGO} content={'This app comes highly recommended by many students in an eCommerce training course I am taking. I have no previous experience and this app has been critical to getting my product landing page in top-notch condition. I have zero coding experience and feel really apprehensive about changing code, so these apps are so useful, and simple for me. Sortecom has gone above and beyond to help me with some adjustments to meet my needs. They even made additional recommendations, which I really loved! I truly appreciate it.'}/>
+                        </CardFeedbackItem>
+                        <CardFeedbackItem>
+                            <CardFeedback name={'LUCY FOLK'} iconUri={IMG_LOGO} content={'I\'ve been using Sortecom\'s apps for a time for different stores & it\'s been amazing. My store speed always remains above 60. And, the support team is brilliant; they\'re always willing to help you. Highly recommended!\n'}/>
+                        </CardFeedbackItem>
+                        <CardFeedbackItem>
+                            <CardFeedback name={'BRIGHTLAND'} iconUri={IMG_LOGO} content={'Absolutely amazing app and if you get stuck or are unsure the customer service is very quick and very helpful! Highly recommend this app -I will be using it for both my websites :)'}/>
+                        </CardFeedbackItem>
+                        <CardFeedbackItem>
+                            <CardFeedback name={'POLAROID'} iconUri={IMG_LOGO} content={'I have been using these Sortecom apps for a while now. Absolutely lovely professional apps. It helps me a lot with quite a range of possibilities, I can\'t say it all. Really really great apps.'}/>
                         </CardFeedbackItem>
                     </FeedbackCardWrapper>
                 </Marquee>
@@ -290,6 +303,8 @@ const CardFeedbackItem = styled.div`
   display: flex;
   flex: 1;
   margin-right: 20px;
+  width: 400px;
+  min-width: 400px;
 `
 const BlogSection = styled.div`
   display: flex;
